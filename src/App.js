0b6b1d6ebb97535d3c10f7000b337cc543dd73d4
Adityavijaysingh2611 <75929995+Adityavijaysingh2611@users.noreply.github.com>
2023-09-29 21:57:43 +0530

    First Commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import Default from "./Components/Default";
+import Header from "./Components/Header";
+// import Footer from "./Footer";
+
+
+function App() {
+  return (
+    <div className="App">
+      {/* <Header/> */}
+      <Default/>
+      
+      {/* <Footer/>  */}
+      
+    </div>
+  );
+}
+
+export default App;
